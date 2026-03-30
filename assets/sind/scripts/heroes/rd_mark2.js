0b0028bf4c74a165963b477b111e48eab9bfec96
RolandasD <137532600+RolandasD@users.noreply.github.com>
2026-03-30 19:48:11 +0200

    RD Mark 2

---
+++ assets/sind/scripts/heroes/rd_mark2.js
@@ -0,0 +1,14 @@
+extend("fiskheroes:hero_basic");
+
+loadTextures({
+    "layer1": "sind:rd_mark2/layer1",
+    "layer2": "sind:rd_mark2/layer2",
+    "lights": "sind:rd_mark2/lights",
+    "extra": "sind:rd_mark2/extra",
+    "mask": "sind:rd_mark2/mask",
+    "hud_mask": "sind:rd_mark2/mask"
+});
+
+function init(hero) {
+    hero.setName("RD Mark 2");
+}
